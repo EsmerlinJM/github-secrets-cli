cd59b8351ee5e92a9efa2e67107b7be98e8320c1
EsmerlinJM <20164379@itla.edu.do>
2022-06-24 18:19:26 -0300

    [ADD]: Code source.

---
+++ cmd/get.go
@@ -0,0 +1,88 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("get called")
+	},
+}
+
+var getOtherCmd = &cobra.Command{
+    Use:   "get-gopher",
+    Short: "This command will get the desired Gopher",
+    Long:  `This get command will call GitHub respository in order to return the desired Gopher.`,
+    Run: func(cmd *cobra.Command, args []string) {
+        var gopherName = "dr-who.png"
+
+        if len(args) >= 1 && args[0] != "" {
+            gopherName = args[0]
+        }
+
+        URL := "https://github.com/scraly/gophers/raw/main/" + gopherName + ".png"
+
+        fmt.Println("Try to get '" + gopherName + "' Gopher...")
+
+        // Get the data
+        response, err := http.Get(URL)
+        if err != nil {
+            fmt.Println(err)
+        }
+        defer response.Body.Close()
+
+        if response.StatusCode == 200 {
+            // Create the file
+            out, err := os.Create(gopherName + ".png")
+            if err != nil {
+                fmt.Println(err)
+            }
+            defer out.Close()
+
+            // Writer the body to file
+            _, err = io.Copy(out, response.Body)
+            if err != nil {
+                fmt.Println(err)
+            }
+
+            fmt.Println("Perfect! Just saved in " + out.Name() + "!")
+        } else {
+            fmt.Println("Error: " + gopherName + " not exists! :-(")
+        }
+    },
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+
+	rootCmd.AddCommand(getOtherCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/root.go
@@ -0,0 +1,72 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "go-github-cli",
+	Short: "Github Secrets CLI in Go",
+	Long:  `Github Screets CLI application written in Go.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-github-secrets-cli.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func init() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".go-gopher-cli" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".go-github-secrets-cli")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
